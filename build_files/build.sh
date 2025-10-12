47f3df15b5d121abe683189ee7d24deaaf3ab5f3
eni <enitimeago@gmail.com>
2025-10-13 03:34:39 +0900

    Swap Envision WiVRn dependencies to envision-wivrn
    7e68cf9 and 64e0ba8 were still not enough. Fedora provides a `envision-wivrn` package which should be more comprehensive.

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,21 +13,22 @@ set -ouex pipefail
 dnf5 install -y tmux 
 
 # Envision 3.2.0 missing build dependencies on top of Bazzite DX
-# Workaround for
-# > package sdl2-compat-devel-2.32.52-1.fc42.x86_64 from fedora requires pkgconfig(glu), but none of the providers can be installed
-# > package mesa-libGLU-devel-9.0.3-6.fc42.x86_64 from fedora is filtered out by exclude filtering
+# For "Lighthouse Driver - Envision Default"
+## mesa-libGLU-devel from fedora satisfies pkgconfig(glu) but is filtered out by exclude filtering
 dnf5 --setopt=disable_excludes=* install -y mesa-libGLU-devel
-# Workaround for
-# > package mesa-libGL-devel-25.1.9-1.fc42.x86_64 from updates-archive requires (mesa-libGL(x86-64) = 25.1.9-1.fc42 if mesa-libGL(x86-64)), but none of the providers can be installed
-# > package mesa-libGL-25.1.9-1.fc42.x86_64 from updates-archive is filtered out by exclude filtering
+## mesa-libGL-devel from updates-archive satisfies mesa-libGL(x86-64) but is filtered out by exclude filtering
 dnf5 --setopt=disable_excludes=* install -y mesa-libGL-devel
-# For "Lighthouse Driver - Envision Default" (excluding mesa-libGL-devel, since already installed above)
+## Remaining dependencies provided by Envision missing dependencies error
 dnf5 install -y eigen3-devel glslang-devel glslc libbsd-devel systemd-devel libusb1 libusb1-devel libXrandr-devel ninja-build openxr-devel SDL2-devel wayland-devel wayland-protocols-devel
 # For "WiVRn - Envision Default"
-# (See https://rpmfusion.org/Howto/Multimedia regarding ffmpeg)
+## Fully replace ffmpeg-free with ffmpeg https://rpmfusion.org/Howto/Multimedia
 dnf5 install -y --allowerasing --from-repo=rpmfusion-free,rpmfusion-free-updates,rpmfusion-nonfree,rpmfusion-nonfree-updates ffmpeg-devel
+## x264-devel comes from RPM Fusion
 dnf5 install -y --from-repo=rpmfusion-free,rpmfusion-free-updates,rpmfusion-nonfree,rpmfusion-nonfree-updates x264-devel
-dnf5 install -y avahi-devel avahi-glib-devel boost-devel cli11-devel eigen3-devel glib2-devel glib2-devel glslang-devel gstreamer1-plugins-base-devel gstreamer1-devel libnotify-devel pipewire-devel systemd-devel librsvg2-devel libva-devel libXrandr-devel ninja-build json-devel openxr-devel patch systemd-devel
+## bluez-libs-devel from fedora satisfies pkgconfig(bluez) but is filtered out by exclude filtering
+dnf5 --setopt=disable_excludes=* install -y bluez-libs-devel
+## Remaining dependencies packaged by Fedora
+dnf5 install -y envision-wivrn
 
 # Use a COPR Example:
 #
