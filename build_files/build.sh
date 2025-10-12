180e4183688018f5b713d3c5c8c4dc5eb7102fd7
eni <enitimeago@gmail.com>
2025-10-13 01:38:23 +0900

    wivrn-dashboard doesn't have Lighthouse, use Envision

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,13 +12,11 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
-# WiVRn + Lighthouse driver
-# https://lvra.gitlab.io/docs/fossvr/wivrn/
-# > These packages come with lighthouse support enabled (Flatpak & Envision does NOT)
-# > ...
-# > - Fedora: Install wivrn-dashboard via DNF
-# > ...
-dnf5 install -y wivrn-dashboard
+# Envision 3.2.0 missing build dependencies on top of Bazzite DX
+# For "Lighthouse Driver - Envision Default"
+dnf5 install -y eigen3-devel glslang-devel glslc libbsd-devel systemd-devel libusb1 libusb1-devel libXrandr-devel mesa-libGL-devel ninja-build openxr-devel SDL2-devel wayland-devel wayland-protocols-devel
+# For "WiVRn - Envision Default"
+dnf5 install -y avahi-devel avahi-glib-devel cli11-devel eigen3-devel glib2-devel glib2-devel glslang-devel gstreamer1-plugins-base-devel gstreamer1-devel ffmpeg-devel ffmpeg-devel ffmpeg-devel libnotify-devel pipewire-devel ffmpeg-devel systemd-devel libva-devel libXrandr-devel ninja-build json-devel openxr-devel patch systemd-devel x264-devel
 
 # Use a COPR Example:
 #
