47dc7c7731aa1daf2d3a71948462e6bee6d34912
eni <enitimeago@gmail.com>
2025-10-13 12:44:33 +0900

    Remove extraneous tmux install from template

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,9 +9,6 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
 # Envision 3.2.0 missing build dependencies on top of Bazzite DX
 # For "Lighthouse Driver - Envision Default"
 ## mesa-libGLU-devel from fedora satisfies pkgconfig(glu) but is filtered out by exclude filtering
