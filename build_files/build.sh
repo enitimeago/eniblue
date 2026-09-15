4479aa5fc6bd21839db675736f051f0ffb33d2cf
eni <enitimeago@gmail.com>
2026-09-16 00:52:30 +0900

    Add .NET SDK 10.0

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,6 +9,9 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# .NET SDK
+dnf5 install -y dotnet-sdk-10.0
+
 # LACT
 dnf5 -y copr enable ilyaz/LACT
 dnf5 install -y lact
